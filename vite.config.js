9157c507185bdd2c26f77eb7d725972664841c54
Florent gravin <florent.gravin@camptocamp.com>
2023-02-08 18:08:24 +0100

    chore: remove hash for wc build

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,11 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/header.js`,
+      },
+    },
+  },
 })
