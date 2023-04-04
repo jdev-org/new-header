dd84ba87510bfe3a10b2ae3837b7d8a93b5e8757
Olivier Guyot <olivier.guyot@camptocamp.com>
2023-04-04 22:37:55 +0200

    chore: fix type error

--- src/auth.ts
+++ src/auth.ts
@@ -41,34 +41,42 @@ export async function getUserDetails(): Promise<User> {
 }
 
 export function affectRoles(user: User): AdminMap {
-  return user.roles.reduce((roles, role) => {
-    switch (role) {
-      case 'ROLE_SUPERUSER':
-      case 'ROLE_ORGADMIN':
-        return {
-          ...roles,
-          admin: true,
-          console: true,
-        }
-      case 'ROLE_GN_ADMIN':
-        return {
-          ...roles,
-          admin: true,
-          catalog: true,
-        }
-      case 'ROLE_ADMINISTRATOR':
-        return {
-          ...roles,
-          admin: true,
-        }
-      case 'ROLE_MAPSTORE_ADMIN':
-        return {
-          ...roles,
-          admin: true,
-          mapstore: true,
-        }
-      default:
-        return roles
-    }
-  }, {})
+  return user.roles.reduce(
+    (roles, role) => {
+      switch (role) {
+        case 'ROLE_SUPERUSER':
+        case 'ROLE_ORGADMIN':
+          return {
+            ...roles,
+            admin: true,
+            console: true,
+          }
+        case 'ROLE_GN_ADMIN':
+          return {
+            ...roles,
+            admin: true,
+            catalog: true,
+          }
+        case 'ROLE_ADMINISTRATOR':
+          return {
+            ...roles,
+            admin: true,
+          }
+        case 'ROLE_MAPSTORE_ADMIN':
+          return {
+            ...roles,
+            admin: true,
+            mapstore: true,
+          }
+        default:
+          return roles
+      }
+    },
+    {
+      admin: false,
+      console: false,
+      catalog: false,
+      mapstore: false,
+    } as AdminMap
+  )
 }
