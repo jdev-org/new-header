d7c12cbf6cb13cea5a57bdaf859b37d1cd7c9a21
Florian Necas <florian.necas@camptocamp.com>
2023-10-05 17:43:55 +0200

    feat: improve mobile menu

--- src/header.ce.vue
+++ src/header.ce.vue
@@ -14,6 +14,7 @@ const props = defineProps<{
 
 const state = reactive({
   user: null as null | User,
+  mobileMenuOpen: false,
 })
 
 const isAnonymous = computed(() => !state.user || state.user.anonymous)
@@ -26,6 +27,10 @@ const loginUrl = computed(() => {
   return current.toString()
 })
 const logoutUrl = computed(() => '/logout')
+
+function toggleMenu(): void {
+  state.mobileMenuOpen = !state.mobileMenuOpen
+}
 
 onMounted(() => {
   getUserDetails().then(user => {
@@ -68,7 +73,7 @@ onMounted(() => {
         >analytics</a
       >
     </div>
-    <div class="h-20 flex justify-between text-slate-600">
+    <div class="h-20 justify-between text-slate-600 sm:flex hidden">
       <div class="flex">
         <a href="/" class="flex justify-center items-center px-8 bg-primary/10">
           <img
@@ -94,6 +99,77 @@ onMounted(() => {
           <a class="link-btn" :href="logoutUrl">logout</a>
         </div>
         <a v-else class="btn" :href="loginUrl">login</a>
+      </div>
+    </div>
+    <div class="flex-col sm:hidden w-full">
+      <div
+        class="h-12 inline-flex items-center justify-start align-middle px-6 py-8 shrink-0 w-full bg-primary/10"
+      >
+        <button
+          type="button"
+          class="grow flex justify-start items-center py-3"
+          @click="toggleMenu"
+        >
+          <div class="inline-flex items-center rounded-full">
+            <svg
+              v-if="state.mobileMenuOpen"
+              xmlns="http://www.w3.org/2000/svg"
+              height="24"
+              viewBox="0 -960 960 960"
+              width="24"
+            >
+              <path
+                d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"
+              />
+            </svg>
+            <svg
+              v-else
+              xmlns="http://www.w3.org/2000/svg"
+              height="24"
+              viewBox="0 -960 960 960"
+              width="24"
+            >
+              <path
+                d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"
+              />
+            </svg>
+            <img
+              src="https://www.georchestra.org/public/georchestra-logo.svg"
+              alt="geOrchestra logo"
+              class="w-24 ml-4"
+            />
+          </div>
+        </button>
+        <div class="flex justify-center items-center">
+          <div v-if="!isAnonymous" class="flex gap-4 items-baseline">
+            <a class="link-btn" href="/console/account/userdetails">
+              <UserIcon class="font-bold text-3xl inline-block mr-4"></UserIcon>
+              <span>{{ state.user?.username }}</span></a
+            >
+            <a class="link-btn" :href="logoutUrl">logout</a>
+          </div>
+          <a v-else class="btn" :href="loginUrl">login</a>
+        </div>
+      </div>
+
+      <div
+        :class="[
+          { 'opacity-100': state.mobileMenuOpen },
+          { 'opacity-0': !state.mobileMenuOpen },
+          'w-full border-b-2 duration-300 transition-all ease-in-out',
+        ]"
+      >
+        <nav class="flex flex-col font-semibold" v-if="state.mobileMenuOpen">
+          <a class="nav-item border-b leading-[3]" href="/datahub/">Data</a>
+          <a class="nav-item border-b leading-[3]" href="/mapstore/">Viewer</a>
+          <a class="nav-item border-b leading-[3]" href="/mapstore/#/home"
+            >Maps</a
+          >
+          <a class="nav-item border-b leading-[3]" href="/geoserver/"
+            >Services</a
+          >
+          <a v-if="!isAnonymous" class="nav-item" href="/import/">Import</a>
+        </nav>
       </div>
     </div>
   </header>
@@ -124,6 +200,9 @@ onMounted(() => {
   }
   .link-btn {
     @apply text-primary/70 hover:underline underline-offset-8 decoration-2 decoration-primary/50;
+  }
+  * {
+    -webkit-tap-highlight-color: transparent;
   }
 }
 </style>
